4feb18e3889c2552616c9200ec18fd3fea355f59
Deirdre  Sullivan <dsulliv4@binghamton.edu>
2019-04-12 06:33:47 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,30 @@
 def division(num1, num2)
- 
+  puts num1 / num2
 end
 
 def assign_variable(value)
-  
+  puts value
 end
 
-def argue
-  
+def argue (word)
+  puts word
 end
 
-def greeting
-
+def greeting(greeting, name)
+  puts "#{greeting} #{name}"
 end
 
 def return_a_value
-
+  puts "Nice"
+  returns "Nice"
 end
 
-def last_evaluated_value
-
+def last_evaluated_value(phrase="expert")
+  puts phrase
+  returns "expert"
 end
 
-def pizza_party
-
+def pizza_party(phrase="cheese")
+  puts phrase
+  returns "cheese"
 end
